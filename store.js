1c405add215152f65a6d72f42648c85954853d85
Sushovan <sushovanghosh960@gmail.com>
2024-04-18 21:08:55 +0530

    create redux store and use Provider to wrap around the entire App

---
+++ store.js
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+// importing the default reducers by giving them corresponding names
+import accountReducer from "./src/slices/accountSlice";
+import bonusReducer from "./src/slices/bonusSlice";
+
+const myStore = configureStore({
+    reducers: {
+        account: accountReducer,
+        bonus: bonusReducer,
+    }
+});
+
+export default myStore;
