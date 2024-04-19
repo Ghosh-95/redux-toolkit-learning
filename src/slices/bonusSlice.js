1318e08f663b496c4194dc0dd75e1248ccc652e7
Sushovan <sushovanghosh960@gmail.com>
2024-04-19 17:49:40 +0530

    use extraReducers to handle points based on the incremented amount
    create an action with 'createAction' to addCase in the builder notaion of extraReducers

--- src/slices/bonusSlice.js
+++ src/slices/bonusSlice.js
@@ -1,4 +1,6 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createAction, createSlice } from "@reduxjs/toolkit"
+
+const incrementByAmount = createAction('account/incrementByAmt');
 
 const bonusSlice = createSlice({
     name: 'bonus',
@@ -10,6 +12,16 @@ const bonusSlice = createSlice({
             state.points += 1;
         },
     },
+
+    extraReducers: (builder) => {
+        builder.addCase(incrementByAmount, (state, action) => {
+            if (action.payload >= 100) {
+                state.points += Math.floor(action.payload / 100);
+            } else {
+                state.points -= 1;
+            };
+        })
+    }
 });
 
 export const { increment } = bonusSlice.actions;
